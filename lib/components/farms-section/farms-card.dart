685c199ccf34a029fbbb7230e0bb74c396e7a85e
Elvis Sarfo Antwi <sarfoantwielvis@gmail.com>
2021-03-27 01:49:16 +0000

    New UI Update from Kanta

--- lib/components/farms-section/farms-card.dart
+++ lib/components/farms-section/farms-card.dart
@@ -5,15 +5,14 @@ import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 
-
 class FarmsCard extends StatefulWidget {
   const FarmsCard(
       {Key key,
-        this.index,
-        this.press,
-        @required this.ht,
-        this.farms,
-        @required this.width})
+      this.index,
+      this.press,
+      @required this.ht,
+      this.farms,
+      @required this.width})
       : super(key: key);
 
   final int index;
@@ -40,7 +39,6 @@ class _FarmsCardState extends State<FarmsCard> {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -63,17 +61,7 @@ class _FarmsCardState extends State<FarmsCard> {
             Radius.circular(15),
           ),
           boxShadow: [
-<<<<<<< HEAD
             isHover ? kDefaultCardHoverShadow : kDefaultCardShadow,
-=======
-            isHover
-                ? kDefaultShadow
-                : BoxShadow(
-              offset: Offset(0, 0),
-              blurRadius: 0,
-              color: primary.withOpacity(.5),
-            ),
->>>>>>> parent of 8bfa782 (Update the UI)
           ],
         ),
         child: Column(
@@ -82,7 +70,6 @@ class _FarmsCardState extends State<FarmsCard> {
           children: [
             Container(
               width: double.infinity,
-<<<<<<< HEAD
               child: ClipRRect(
                 borderRadius: BorderRadius.only(
                   topRight: Radius.circular(14),
@@ -211,86 +198,6 @@ class _FarmsCardState extends State<FarmsCard> {
                                 fontWeight: FontWeight.w600,
                                 color: Colors.grey,
                                 fontSize: 16),
-=======
-                child: Image.asset(widget.farms.images[0],fit: BoxFit.fill,height: 150,)),
-            SizedBox(
-              height: 5,
-            ),
-            ListTile(
-              title: Padding(
-                padding: const EdgeInsets.only(bottom: 5),
-                child: Text("Farm Details",style: TextStyle(fontWeight: FontWeight.w700,color: primary,fontSize: 14,decoration: TextDecoration.underline),),
-              ),
-              subtitle: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 18,bottom: 10,left: 18),
-                    child: Row(
-                      children: [
-                        Text("Farm ID: ",style: TextStyle(fontWeight: FontWeight.w700,color: primaryLight,fontSize: 13),),
-                        Text(widget.farms.farmId,style: TextStyle(fontWeight: FontWeight.w700,color:Colors.grey ,fontSize: 13),),
-                      ],
-                    ),
-                  ),
-                  ListTile(
-                    title:Padding(
-                      padding: const EdgeInsets.only(bottom: 5),
-                      child: Text("Description:",style: TextStyle(fontWeight: FontWeight.w700,color: primaryLight,fontSize: 13),),
-                    ),
-                    subtitle: Text(widget.farms.description,
-                      maxLines : 3,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontWeight: FontWeight.bold,color:Colors.grey,fontSize: 13),),
-                  ),
-                  ListTile(
-                    title: Padding(
-                      padding: const EdgeInsets.only(bottom: 8),
-                      child: Text("Crops on Farm:",style: TextStyle(fontWeight: FontWeight.w700,color: primaryLight,fontSize: 13),),
-                    ),
-                    subtitle:  Wrap(
-                        crossAxisAlignment: WrapCrossAlignment.start,
-                        alignment: WrapAlignment.start,
-                        children: List.generate(
-                          widget.farms.cropType.length,
-                              (index) => Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 5),
-                            child: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                CircleAvatar(backgroundColor: Colors.grey,radius: 4,),
-                                SizedBox(width: 4,),
-                                Text(widget.farms.cropType[index],style: TextStyle(fontSize: 13,fontWeight:FontWeight.bold,color:Colors.grey),),
-                              ],
-                            ),
-                          ),)
-                    ),
-                  ),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: ListTile(
-                          title:Padding(
-                            padding: const EdgeInsets.only(bottom: 5),
-                            child: Text("Location:",style: TextStyle(fontWeight: FontWeight.w700,color: primaryLight,fontSize: 13),),
-                          ),
-                          subtitle: Padding(
-                            padding: const EdgeInsets.only(left:8.0),
-                            child: Text(widget.farms.location!=null?widget.farms.location:"",
-                              style: TextStyle(fontWeight: FontWeight.bold,color:Colors.grey,fontSize: 13),),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        child: ListTile(
-                          title:Padding(
-                            padding: const EdgeInsets.only(bottom: 5),
-                            child: Text("Farm Size:",style: TextStyle(fontWeight: FontWeight.w700,color: primaryLight,fontSize: 13),),
-                          ),
-                          subtitle: Padding(
-                            padding: const EdgeInsets.only(left:8.0),
-                            child: Text(widget.farms.farmSize.toString()!=null?widget.farms.farmSize.toString()+" Acr":"",
-                              style: TextStyle(fontWeight: FontWeight.bold,color:Colors.grey,fontSize: 13),),
->>>>>>> parent of 8bfa782 (Update the UI)
                           ),
                         ),
                       ),
@@ -308,7 +215,6 @@ class _FarmsCardState extends State<FarmsCard> {
                           fontSize: 14),
                     ),
                   ),
-<<<<<<< HEAD
                   subtitle: Text(
                     widget.farms.description,
                     maxLines: 2,
@@ -322,13 +228,7 @@ class _FarmsCardState extends State<FarmsCard> {
                   ),
                 ),
               ],
-=======
-
-                ],
-              ),
->>>>>>> parent of 8bfa782 (Update the UI)
             ),
-
           ],
         ),
       ),
